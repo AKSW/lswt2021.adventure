175cc3f87fb748e97a2e0b72221b3c9264ac750a
Natanael Arndt <arndtn@gmail.com>
2021-06-21 16:46:21 +0200

    Add logos

---
+++ LSWT.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="LSWT2" tilewidth="32" tileheight="32" tilecount="132" columns="22">
+ <image source="LSWT_Tiles.png" width="704" height="192"/>
+</tileset>
